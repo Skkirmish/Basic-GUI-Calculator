4f219f94e1d3b93a2441984f67a432a5f32e87a5
Skkirmish <emiliodelosreyes123@gmail.com>
2023-07-02 14:51:09 -0500

    commit

---
+++ Basic GUI Calculator.py
@@ -0,0 +1,105 @@
+import tkinter as tk
+
+calculation = ""
+
+#Specify Functions of application
+def add_to_calculation(symbol):
+    global calculation
+    calculation += str(symbol)
+    text_result.delete(1.0, "end")
+    text_result.insert(1.0, calculation)
+
+def evaluate_calculation():
+    global calculation
+    try:
+        calculation = str(eval(calculation))
+        text_result.delete(1.0, "end")
+        text_result.insert(1.0, calculation)
+    except:
+        clear_field()
+        text_result.insert(1.0, "Error")
+
+def clear_field():
+    global calculation
+    calculation = ""
+    text_result.delete(1.0, "end")
+
+
+root = tk.Tk()
+root.geometry("300x275")
+
+text_result = tk.Text(root, height=2, width=16, font=("Arial", 24))
+text_result.grid(columnspan=5)
+
+#Button 1
+btn_1 = tk.Button(root, text="1", command=lambda:add_to_calculation(1), width=5, font=("Arial", 14))
+btn_1.grid(row=2, column=1)
+
+#Button 2
+btn_2 = tk.Button(root, text="2", command=lambda:add_to_calculation(2), width=5, font=("Arial", 14))
+btn_2.grid(row=2, column=2)
+
+#Button 3
+btn_3 = tk.Button(root, text="3", command=lambda:add_to_calculation(3), width=5, font=("Arial", 14))
+btn_3.grid(row=2, column=3)
+
+#Button 4
+btn_4 = tk.Button(root, text="4", command=lambda:add_to_calculation(4), width=5, font=("Arial", 14))
+btn_4.grid(row=3, column=1)
+
+#Button 5
+btn_5 = tk.Button(root, text="5", command=lambda:add_to_calculation(5), width=5, font=("Arial", 14))
+btn_5.grid(row=3, column=2)
+
+#Button 6
+btn_6 = tk.Button(root, text="6", command=lambda:add_to_calculation(6), width=5, font=("Arial", 14))
+btn_6.grid(row=3, column=3)
+
+#Button 7
+btn_7 = tk.Button(root, text="7", command=lambda:add_to_calculation(7), width=5, font=("Arial", 14))
+btn_7.grid(row=4, column=1)
+
+#Button 8
+btn_8 = tk.Button(root, text="8", command=lambda:add_to_calculation(8), width=5, font=("Arial", 14))
+btn_8.grid(row=4, column=2)
+
+#Button 9
+btn_9 = tk.Button(root, text="9", command=lambda: add_to_calculation(9), width=5, font=("Arial", 14))
+btn_9.grid(row=4, column=3)
+
+#Button 10
+btn_0 = tk.Button(root, text="0", command=lambda: add_to_calculation(0), width=5, font=("Arial", 14))
+btn_0.grid(row=5, column=2)
+
+#Plus button
+btn_plus = tk.Button(root, text="+", command=lambda: add_to_calculation("+"), width=5, font=("Arial", 14))
+btn_plus.grid(row=2, column=4)
+
+#Minus button
+btn_minus = tk.Button(root, text="-", command=lambda: add_to_calculation("-"), width=5, font=("Arial", 14))
+btn_minus.grid(row=3, column=4)
+
+#Multiplication button
+btn_multiply = tk.Button(root, text="*", command=lambda: add_to_calculation("*"), width=5, font=("Arial", 14))
+btn_multiply.grid(row=4, column=4)
+
+#Division button
+btn_divide = tk.Button(root, text="÷", command=lambda: add_to_calculation("÷"), width=5, font=("Arial", 14))
+btn_divide.grid(row=5, column=4)
+
+#Open button
+btn_open = tk.Button(root, text="(", command=lambda: add_to_calculation("("), width=5, font=("Arial", 14))
+btn_open.grid(row=5, column=1)
+
+#Close botton
+btn_close = tk.Button(root, text=")", command=lambda: add_to_calculation(")"), width=5, font=("Arial", 14))
+btn_close.grid(row=5, column=3)
+
+#Clear button
+btn_clear = tk.Button(root, text="C", command= clear_field, width=11, font=("Arial", 14))
+btn_clear.grid(row=6, column=1, columnspan=2)
+
+#Equals button
+btn_equal = tk.Button(root, text="=", command= evaluate_calculation, width=11, font=("Arial", 14))
+btn_equal.grid(row=6, column=3, columnspan=2)
+root.mainloop()
